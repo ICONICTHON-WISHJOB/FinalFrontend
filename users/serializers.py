e1d0b1dc82329a700622480389701cc89e73a975
HANSUMIN <emamgin@gmail.com>
2024-11-14 18:55:28 +0900

    cors 1

--- users/serializers.py
+++ users/serializers.py
@@ -1,12 +1,16 @@
 from rest_framework import serializers
 from django.contrib.auth.hashers import make_password
-from .models import CustomUser
+from . import models
 
 class SignupSerializer(serializers.ModelSerializer):
     class Meta:
-        model = CustomUser
+        model = models.CustomUser
         fields = ['email', 'password', 'phoneNum', 'first_name', 'birth']
 
     def create(self, validated_data):
         validated_data['password'] = make_password(validated_data['password'])
         return super().create(validated_data)
+
+class LoginSerializer(serializers.Serializer):
+    email = serializers.CharField(required=True)
+    password = serializers.CharField(required=True, write_only=True)
--- users/views.py
+++ users/views.py
@@ -1,9 +1,30 @@
+from .serializers import SignupSerializer
+from .models import CustomUser
+
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.authtoken.models import Token
-from .serializers import SignupSerializer
-from .models import CustomUser
+from django.contrib.auth import authenticate
+from .serializers import LoginSerializer
+
+
+class LoginView(APIView):
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+        if serializer.is_valid():
+            email = serializer.validated_data['email']
+            password = serializer.validated_data['password']
+            user = authenticate(username=email, password=password)
+
+            if user is not None:
+                token, _ = Token.objects.get_or_create(user=user)
+                return Response({'token': token.key}, status=status.HTTP_200_OK)
+            else:
+                return Response({'error': 'Invalid email or password'}, status=status.HTTP_400_BAD_REQUEST)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 class SignupView(APIView):
     def post(self, request):
