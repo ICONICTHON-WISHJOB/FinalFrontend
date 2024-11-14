cae07cd1d009aca51c055df931f8b23d9fab7fde
HANSUMIN <emamgin@gmail.com>
2024-11-15 08:27:51 +0900

    url 수정

--- jobfair/urls.py
+++ jobfair/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('reserve-booth/<int:booth_id>/', views.ReserveBoothView.as_view(), name='reserve_booth'),
-    path('queue-position/<int:booth_id>/', views.CheckQueuePositionView.as_view(), name='check_queue_position'),
+    path('reserve-booth/<str:booth_id>/', views.ReserveBoothView.as_view(), name='reserve_booth'),
+    path('queue-position/<str:booth_id>/', views.CheckQueuePositionView.as_view(), name='check_queue_position'),
     path('boothList/<str:day>/<str:floor>/', views.BoothListView.as_view(), name='booth_list'),
     path('boothApply/', views.BoothApplyView.as_view(), name='booth_apply'),
     path('boothPossibleNow/', views.BoothPossibleNowView.as_view(), name='booth_possible'),
